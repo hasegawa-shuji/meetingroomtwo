9a0ba8729308fa2ce02ea1f3fbdfd141b68ceaf0
hasegawashuji <hasegawa-sh-01@sankei-gk.co.jp>
2023-10-26 11:01:33 +0900

    django3.2

--- pj_meetingroom3/settings.py
+++ pj_meetingroom3/settings.py
@@ -139,7 +139,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-# USE_TZ = True
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
@@ -151,7 +151,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
